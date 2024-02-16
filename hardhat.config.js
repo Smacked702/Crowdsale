210817f49543deb8019c205d11bd344135ad9c86
Rob D <rnthat.702@gmail.com>
2024-02-15 22:35:36 -0800

    Add smart contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+    networks: {
+      hardhat: {
+        // Configuration options
+      },
+      local: {
+        url: "http://127.0.0.1:8545"
+      }
+    },
+    solidity: "0.8.0",
 };
